b2ff8324f47354cd6f8d2f70a0185f575e6bae74
deepakbhattmits <deepakbhattmits@gmail.com>
2021-11-06 18:48:25 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { render } from "react-dom";
 
 import App from "./App";
-import './styles.css'
+import './assets/styles/_index.scss'
 
 const rootElement = document.getElementById("root");
 render(<App />, rootElement);
