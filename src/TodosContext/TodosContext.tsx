9cf906664bb81011d488abb3bcdcddcdb8bbd4e1
deepakbhattmits <deepakbhattmits@gmail.com>
2021-11-16 08:40:22 +0530

    Update TodosContext.tsx

--- src/TodosContext/TodosContext.tsx
+++ src/TodosContext/TodosContext.tsx
@@ -1,50 +1,49 @@
-import  { createContext, useState,useEffect, FC } from "react";
-import {TodosContextState, Itodos} from "./types";
+import { createContext, useState, useEffect, FC } from 'react'
+import { TodosContextState, Itodos } from './types'
 
 const contextDefaultValues: TodosContextState = {
   todos: [],
-  addTodo: () => { },
-  removeTodo: () => { },
-  markCompleted:()=>{}
-};
+  addTodo: () => {},
+  removeTodo: () => {},
+  markCompleted: () => {},
+}
 
 export const TodosContext = createContext<TodosContextState>(
-  contextDefaultValues
-);
+  contextDefaultValues,
+)
 
-const TodosProvider: FC=({children}) =>
-{
-  const [todos,setTodos]=useState<Itodos[]>(contextDefaultValues?.todos);
-  const addTodo=(title: string) =>
-  {
-    console.log('ADD TODO ',todos,title);
-    setTodos((todos: Itodos[]) => [...todos,{id: Math?.random()?.toString(),title,completed:false}]);
+const TodosProvider: FC = ({ children }) => {
+  const [todos, setTodos] = useState<Itodos[]>(contextDefaultValues?.todos)
+  const addTodo = (title: string) => {
+    console.log('ADD TODO ', todos, title)
+    setTodos((todos: Itodos[]) => [
+      ...todos,
+      { id: Math?.random()?.toString(), title, completed: false },
+    ])
   }
-  const removeTodo=(id: string) =>
-  {
-    const updatedTodos=todos?.filter(el => el?.id!==id);
-    setTodos(updatedTodos);
+  const removeTodo = (id: string) => {
+    const updatedTodos = todos?.filter((el) => el?.id !== id)
+    setTodos(updatedTodos)
   }
-  const markCompleted=(id: string) =>
-  {
-    const updatedTodo=todos?.map(el => el?.id===id? {...el,completed: !el?.completed}:el);
-      setTodos(updatedTodo);
+  const markCompleted = (id: string) => {
+    const updatedTodo = todos?.map((el) =>
+      el?.id === id ? { ...el, completed: !el?.completed } : el,
+    )
+    setTodos(updatedTodo)
   }
 
-  useEffect(() =>
-  {
-    if(todos?.length) {
-      localStorage.setItem('todos',JSON.stringify(todos));
+  useEffect(() => {
+    if (todos?.length) {
+      localStorage.setItem('todos', JSON.stringify(todos))
     }
-  },[todos]);
-  useEffect(() =>
-  {
-    const todosLocal=JSON.parse(localStorage.getItem('todos')||'{}');
-    
-    if(!todos?.length&& todosLocal?.length) {
-      setTodos(todosLocal)     
+  }, [todos])
+  useEffect(() => {
+    const todosLocal = JSON.parse(localStorage.getItem('todos') || '{}')
+
+    if (!todos?.length && todosLocal?.length) {
+      setTodos(todosLocal)
     }
-  },[todos]);
+  }, [todos])
 
   return (
     <TodosContext.Provider
@@ -52,12 +51,12 @@ const TodosProvider: FC=({children}) =>
         todos,
         addTodo,
         removeTodo,
-        markCompleted
+        markCompleted,
       }}
     >
       {children}
     </TodosContext.Provider>
-  );
-};
+  )
+}
 
-export default TodosProvider;
+export default TodosProvider
