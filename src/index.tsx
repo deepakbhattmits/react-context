cefea9e16a07c2de74b4a6402b6e102b5eca57ef
deepakbhattmits <deepakbhattmits@gmail.com>
2022-05-03 16:42:57 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
-import { render } from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 import App from './App'
 import './assets/styles/_index.scss'
 
-const rootElement = document.getElementById('root')
-render(<App />, rootElement)
+const rootElement = document.getElementById('root') as HTMLDivElement
+const root = createRoot(rootElement)
+root?.render(<App />)
