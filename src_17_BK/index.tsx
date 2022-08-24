4572f36974b4eaff111d33c60c33e6afb3147cb7
deepakbhattmits <deepakbhattmits@gmail.com>
2022-08-24 10:39:20 +0530

    Create index.tsx

---
+++ src_17_BK/index.tsx
@@ -0,0 +1,7 @@
+import { render } from 'react-dom'
+
+import App from './App'
+import './assets/styles/_index.scss'
+
+const rootElement = document.getElementById('root')
+render(<App />, rootElement)
